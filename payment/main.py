955067f92dd82643e998dc2927e65a51b061ce13
tannowenn <owen.tan.2022@scis.smu.edu.sg>
2024-03-16 17:17:23 +0800

    setting up db link in main

--- payment/main.py
+++ payment/main.py
@@ -5,12 +5,44 @@ from dotenv import load_dotenv
 import stripe
 
 from flask import Flask, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
+from datetime import datetime
 
 # Load stripe test API key
 load_dotenv()
 stripe.api_key = os.getenv('STRIPE_KEY')
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Payment(db.Model):
+    __tablename__ = 'payment'
+
+    paymentId = db.Column(db.Integer, primary_key=True)
+    rentalId = db.Column(db.String(32), nullable=False)
+    payerId = db.Column(db.String(32), nullable=False)
+    payeeId = db.Column(db.String(32), nullable=False)
+    amountSgd = db.Column(db.Float(precision=2), nullable=False)
+    status = db.Column(db.String(10), nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    modified = db.Column(db.DateTime, nullable=False,
+                         default=datetime.now, onupdate=datetime.now)
+
+    def __init__(self, paymentId, rentalId, payerId, payeeId, amountSgd, status, created, modified):
+        self.paymentId = paymentId
+        self.rentalId = rentalId
+        self.payerId = payerId
+        self.payeeId = payeeId
+        self.amountSgd = amountSgd
+        self.status = status
+        self.created = created
+        self.modified = modified
+
+    def json(self):
+        return {"paymentId": self.paymentId, "rentalId": self.rentalId, "payerId": self.payerId, "payeeId": self.payeeId, "amountSgd": self.amountSgd, "status": self.status, "created": self.created, "modified": self.modified}
 
 @app.route('/payment/success')
 def success():
