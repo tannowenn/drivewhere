07488be4941c269239326c2a0e5b7da07dc89d4c
Clement-Chong <jjchong.2022@smu.edu.sg>
2024-03-17 20:45:12 +0800

    Update rental.sql

--- rental/rental.sql
+++ rental/rental.sql
@@ -3,13 +3,13 @@ USE `rental`;
 
 DROP TABLE IF EXISTS `rental`;
 CREATE TABLE IF NOT EXISTS `rental` (
-  `rentalId` int NOT NULL AUTO_INCREMENT,
+  `rentalId` INT NOT NULL AUTO_INCREMENT,
   `status` varchar(16) NOT NULL,
   `userId` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `carModel` varchar(255) NOT NULL,
   `carMake` varchar(255) NOT NULL,
-  `capacity` int NOT NULL,
+  `capacity` INT NOT NULL,
   `carPlate` varchar(16) NOT NULL
   PRIMARY KEY (`rentalId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
