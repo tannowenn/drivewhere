b57d18defa61f090ee88fe1fb0be02b55714b9c8
Clement-Chong <jjchong.2022@smu.edu.sg>
2024-03-18 23:19:59 +0800

    import os

--- rental/main.py
+++ rental/main.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 import googlemaps
-from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/rental'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://root@localhost:3306/rental'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
