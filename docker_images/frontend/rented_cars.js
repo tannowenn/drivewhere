692014d070416b55a9484874fdfd290dcc148e8e
tannowenn <owen.tan.2022@scis.smu.edu.sg>
2024-03-31 02:56:37 +0800

    make backend data handling for /rental

--- docker_images/frontend/rented_cars.js
+++ docker_images/frontend/rented_cars.js
@@ -1,6 +1,9 @@
 // when user presses on their rented cars
 // getting data from rental DB
 window.onload = function () {
+    // harded coded value user 3 (which is us rn)
+    currentUserId = "3"
+
     fetch("http://localhost:5002/rental", {
         method: "POST",
         headers: {
@@ -8,7 +11,8 @@ window.onload = function () {
         },
         body: JSON.stringify({
             address: "160139",
-            status: "rented"
+            status: "rented",
+            userId: currentUserId
         })
     })
     .then(response => {
@@ -21,32 +25,25 @@ window.onload = function () {
         console.log("Data:", data.data.rental_list)
         var documentSelector = document.getElementById("rentedCarList")
         const cars = data.data.rental_list
-        
+
         for (i=0; i<cars.length; i++) {
-
-            // harded coded value user 3 (which is us rn)
-            if (cars[i].userId == "3") {
-                var div = document.createElement("div")
-                div.classList.add("col-md-4")
-                div.classList.add("mb-3")
-                var car = `
-                  <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                      <h5 class="card-title">${cars[i].carMake} | ${cars[i].carModel}</h5>
-                      <h6 class="card-subtitle mb-2 text-body-secondary">Capacity: ${cars[i].capacity} | Price per day: ${cars[i].pricePerDay}</h6>
-                      <p class="card-text">Carplate: ${cars[i].carPlate}</p>
-                      <p class="card-footer"><button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#returnCar" onclick="${addReturnRentalId(cars[i].rentalId)}">Return</button></p>
-                      
-                    </div>
-                  </div>
-                `
-                
-                div.innerHTML = car
-                documentSelector.appendChild(div)
-            } 
+            var div = document.createElement("div")
+            div.classList.add("col-md-4")
+            div.classList.add("mb-3")
+            var car = `
+                <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${cars[i].carMake} | ${cars[i].carModel}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">Capacity: ${cars[i].capacity} | Price per day: ${cars[i].pricePerDay}</h6>
+                    <p class="card-text">Carplate: ${cars[i].carPlate}</p>
+                    <p class="card-footer"><button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#returnCar" onclick="${addReturnRentalId(cars[i].rentalId)}">Return</button></p>
+                    
+                </div>
+                </div>
+            `
+            div.innerHTML = car
+            documentSelector.appendChild(div)
         }
-
-        
     })
 }
 
